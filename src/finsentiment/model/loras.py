0a7cc86bf6fda64b1bdd4957a78f385a9eb8bc9c
MaudDK <62085173+MaudDK@users.noreply.github.com>
2026-06-02 07:19:11 -0400

    feat: added LoRA config and application

---
+++ src/finsentiment/model/loras.py
@@ -0,0 +1,22 @@
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from finsentiment.config import load_config
+from finsentiment.model.loader import load_model
+
+def build_lora_config(config_path: str) -> LoraConfig:
+    config = load_config(config_path)
+
+    lora_config = LoraConfig(
+        r=config['lora']['r'],
+        lora_alpha=config['lora']['alpha'],
+        lora_dropout=config['lora']['dropout'],
+        target_modules=config['lora']['target_modules'],
+        bias="none",
+        task_type="CAUSAL_LM"
+    )
+    return lora_config
+
+
+def apply_lora(model, lora_config: LoraConfig):
+    model = prepare_model_for_kbit_training(model)
+    model = get_peft_model(model, lora_config)
+    return model
